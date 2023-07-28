988827d379c3f1416a17877a733912f90160de2e
Suzie <its.suzie.here@gmail.com>
2023-07-28 21:25:16 +0530

    Deleted all configurations , started over

--- web_files/src/index.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCZCkoL1G6O8Cle-kD5bRlrfEKr3qDL1BA",
-    authDomain: "arduino-led-controller-2ab40.firebaseapp.com",
-    projectId: "arduino-led-controller-2ab40",
-    storageBucket: "arduino-led-controller-2ab40.appspot.com",
-    messagingSenderId: "852653960995",
-    appId: "1:852653960995:web:2f6cd51e3f407813e66696"
-};
